f6bcac1e5ea7ff5b8e7d0bd1d8c8fc7636513359
agungjati <agungjati94@gmail.com>
2020-03-13 21:57:24 +0700

    add css navbar, list contact, member in  home

--- src/Home.vue
+++ src/Home.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="container d-flex">
       <div class="d-flex flex-column" style="width: 30%">
-        <div>
-          image
+        <div class="logo">
+          Chatroom
         </div>
-      <div>list chat</div>
+      <div class="list-member">
+        <div class="member">
+           <span>A</span>
+           <div>
+              <span>Agung Jati</span>
+              <small>agungjati94@gmail.com</small>
+           </div>
+        </div>
+        <div class="member">
+           <span>A</span>
+           <div>
+              <span>Agung Jati</span>
+              <small>agungjati94@gmail.com</small>
+           </div>
+        </div>
+      </div>
       </div>
       <div class="d-flex flex-column" style="width: 70%">
-        <div>
+        <div class="navbar">
           image
         </div>
-      <div> chat</div>
+        <div class="chat"> 
+          
+        </div>
       </div>
   </div>
 </template>
@@ -27,14 +44,66 @@ export default {
 </script>
 
 <style scoped>
-.form-signin {
-    width: 100%;
-    max-width: 420px;
-    padding: 15px;
-    margin: auto;
-    height: 100vh;
+.logo {
+  border-right: 3px solid #e06060;
+}
+.navbar {
     display: flex;
+    align-items: center;
+    justify-content: flex-end;text-align: left;
+}
+.logo, .navbar  {
+    background: #f76c6c;
+    color: #fff;
+    padding: 10px 5px;
+    font-weight: bolder;
+}
+
+.list-member {
+  background: #fff2f8;
+  padding: 10px 0;
+}
+
+.list-member, .chat{
+  height: calc(100vh - 24px);
+    overflow: auto;
+}
+
+.member >span {
+    display: flex;
+    background: #b2a7c7;
+    color: #fff;
+    height: 35px;
+    width: 35px;
+    border-radius: 50%;
+    text-align: center;
+    align-items: center;
     justify-content: center;
+}
+
+.member {
+    display: flex;
+    align-items: center;
+    padding: 3px 0 0 10px;
+    color: #544545;
+    margin-bottom: 10px;
+}
+
+.member >div {
+    display: flex;
     flex-direction: column;
+    padding: 5px;
+    flex: 1;
+    border-bottom: 1px solid #999;
+        
+}
+
+.member small{
+    font-size: 11px;
+    color: #666;
+}
+
+.chat {
+  background: #fff2f8;
 }
 </style>
