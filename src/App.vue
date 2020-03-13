816da2a9278a3dc490cb7f8887611615bee204d4
development <development@inforsys.co.id>
2020-03-13 16:13:25 +0700

    fix bug

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Login />
+  </div>
+</template>
+
+<script>
+import Login from './Login'
+
+export default {
+  name: 'app',
+  components: { Login },
+  data () {
+    return {
+      msg: 'hello world'
+    }
+  }
+}
+</script>
+
+<style>
+@import './assets/bootstrap.min.css';
+</style>
